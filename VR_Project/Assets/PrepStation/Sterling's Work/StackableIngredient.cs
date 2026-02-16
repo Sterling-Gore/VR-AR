791dd7826b77914c56a3be531aea30eabfc8882b
Sterling-Gore <155609125+Sterling-Gore@users.noreply.github.com>
2026-02-16 14:33:53 -0600

    feat: initialized StackableIngredient script

---
+++ VR_Project/Assets/PrepStation/Sterling's Work/StackableIngredient.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class StackableIngredient : MonoBehaviour
+{
+    [SerializeField] private Collider aboveCollider;
+    [SerializeField] private Collider belowCollider; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void AttachIngredient()
+    {
+
+    }
+}
