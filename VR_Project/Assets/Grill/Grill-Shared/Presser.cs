d818bee2327464a00333ff6d7a7cd127e7f84d15
axh786 <143470097+axh786@users.noreply.github.com>
2026-02-26 00:16:18 -0600

    Add presser logic
    Fill out some beginner presser logic code heavily based on the previous
    pseudo code

--- VR_Project/Assets/Grill/Grill-Shared/Presser.cs
+++ VR_Project/Assets/Grill/Grill-Shared/Presser.cs
@@ -1,6 +1,20 @@
+using System.Runtime.CompilerServices;
+using System.Security;
 using UnityEngine;
 
-public class Presser
-{
-    
+public class Presser : MonoBehaviour {
+    private Patty cookingPatty;
+
+    private void PresserPattyTouch(Collider other) {
+        Patty patty = other.GetComponent<Patty>(); 
+
+        if (patty != null) {
+            cookingPatty = patty; 
+            cookingPatty.ApplyHeat(Time.deltaTime); 
+        }
+    }
+
+    private void PresserPattyNotTouch(Collider other) {
+        cookingPatty = null;
+    }
 }
