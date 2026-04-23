74961864a32d24a7d6b486874d28637adde53ba1
axh786 <143470097+axh786@users.noreply.github.com>
2026-04-22 20:40:46 -0500

    Revert "Merge pull request #30 from Sterling-Gore/fryer_/double"
    This reverts commit b2b26e719e015ce54e3f8d5b54f011c1b25400a9, reversing
    changes made to d3c6d50dc93ffe99d9b775d1ea284227ce02b651.

--- VR_Project/Assets/FryerStation/Melanie's Work/Basket.cs
+++ VR_Project/Assets/FryerStation/Melanie's Work/Basket.cs
@@ -1,8 +1,5 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class Basket : MonoBehaviour
 {
@@ -16,27 +13,12 @@ public class Basket : MonoBehaviour
     [Header("Basket Visuals")]
     [SerializeField] private float occupiedAlpha = 0.2f;
 
-    [Header("Input")]
-    [SerializeField] private XRGrabInteractable basketGrabInteractable;
-    [SerializeField] private InputActionReference rightControllerDisconnectAction;
-    [SerializeField] private InputActionReference leftControllerDisconnectAction;
-
     private FryItem lockedFry;
-    private FryItem fryInTrigger;
     private Material basketMaterial;
     private Color basketBaseColor = Color.white;
-    private bool leftControllerHoldingBasket;
-    private bool rightControllerHoldingBasket;
-    private float lastToggleTime = -1f;
-    private float toggleCooldownDuration = 0.3f;
 
     private void Awake()
     {
-        if (basketGrabInteractable == null)
-        {
-            basketGrabInteractable = GetComponent<XRGrabInteractable>();
-        }
-
         Renderer renderer = GetComponent<Renderer>();
         if (renderer != null)
         {
@@ -44,20 +26,6 @@ public class Basket : MonoBehaviour
             basketBaseColor = basketMaterial.color;
             SetBasketAlpha(1f);
         }
-    }
-
-    private void OnEnable()
-    {
-        RegisterGrabCallbacks();
-        RegisterDisconnectInputCallbacks();
-    }
-
-    private void OnDisable()
-    {
-        UnregisterGrabCallbacks();
-        UnregisterDisconnectInputCallbacks();
-        leftControllerHoldingBasket = false;
-        rightControllerHoldingBasket = false;
     }
 
     private void Update()
@@ -75,24 +43,18 @@ public class Basket : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (lockedFry != null)
+        {
+            return;
+        }
+
         FryItem fry = other.GetComponentInParent<FryItem>();
         if (fry == null)
         {
             return;
         }
 
-        fryInTrigger = fry;
-        Debug.Log("Fry entered basket trigger. Press trigger to lock.");
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        FryItem fry = other.GetComponentInParent<FryItem>();
-        if (fry != null && fryInTrigger == fry)
-        {
-            fryInTrigger = null;
-            Debug.Log("Fry exited basket trigger.");
-        }
+        LockFryInBasket(fry);
     }
 
     private void LockFryInBasket(FryItem fry)
@@ -142,36 +104,16 @@ public class Basket : MonoBehaviour
         SetBasketAlpha(occupiedAlpha);
     }
 
-    [ContextMenu("Release Fry")]
-    public void ReleaseLockedFry()
+    public void TrashLockedFry()
     {
         if (lockedFry == null)
         {
+            SetBasketAlpha(1f);
             return;
         }
 
-        XRGrabInteractable grabInteractable = lockedFry.GetComponent<XRGrabInteractable>();
-        if (grabInteractable != null)
-        {
-            grabInteractable.enabled = true;
-        }
-
-        Rigidbody fryBody = lockedFry.GetComponent<Rigidbody>();
-        if (fryBody != null)
-        {
-            fryBody.isKinematic = false;
-            fryBody.useGravity = true;
-        }
-
-        Collider[] fryColliders = lockedFry.GetComponentsInChildren<Collider>();
-        foreach (Collider col in fryColliders)
-        {
-            col.enabled = true;
-        }
-
-        lockedFry.transform.SetParent(null, true);
+        Destroy(lockedFry.gameObject);
         lockedFry = null;
-
         SetBasketAlpha(1f);
     }
 
@@ -185,141 +127,5 @@ public class Basket : MonoBehaviour
         Color color = basketBaseColor;
         color.a = Mathf.Clamp01(alpha);
         basketMaterial.color = color;
-    }
-
-    private void RegisterGrabCallbacks()
-    {
-        if (basketGrabInteractable == null)
-        {
-            return;
-        }
-
-        basketGrabInteractable.selectEntered.AddListener(OnBasketGrabbed);
-        basketGrabInteractable.selectExited.AddListener(OnBasketReleased);
-    }
-
-    private void UnregisterGrabCallbacks()
-    {
-        if (basketGrabInteractable == null)
-        {
-            return;
-        }
-
-        basketGrabInteractable.selectEntered.RemoveListener(OnBasketGrabbed);
-        basketGrabInteractable.selectExited.RemoveListener(OnBasketReleased);
-    }
-
-    private void OnBasketGrabbed(SelectEnterEventArgs args)
-    {
-        IXRSelectInteractor interactor = args.interactorObject;
-        Transform interactorTransform = interactor != null ? interactor.transform : null;
-        string tag = interactorTransform != null ? interactorTransform.tag : string.Empty;
-        string name = interactorTransform != null ? interactorTransform.name.ToLowerInvariant() : string.Empty;
-
-        bool isLeft = tag == "LeftHand" || name.Contains("left");
-        bool isRight = tag == "RightHand" || name.Contains("right");
-
-        leftControllerHoldingBasket = isLeft;
-        rightControllerHoldingBasket = isRight;
-    }
-
-    private void OnBasketReleased(SelectExitEventArgs args)
-    {
-        leftControllerHoldingBasket = false;
-        rightControllerHoldingBasket = false;
-    }
-
-    private void RegisterDisconnectInputCallbacks()
-    {
-        if (rightControllerDisconnectAction != null && rightControllerDisconnectAction.action != null)
-        {
-            rightControllerDisconnectAction.action.started += OnRightDisconnectPressed;
-        }
-
-        if (leftControllerDisconnectAction != null && leftControllerDisconnectAction.action != null)
-        {
-            leftControllerDisconnectAction.action.started += OnLeftDisconnectPressed;
-        }
-    }
-
-    private void UnregisterDisconnectInputCallbacks()
-    {
-        if (rightControllerDisconnectAction != null && rightControllerDisconnectAction.action != null)
-        {
-            rightControllerDisconnectAction.action.started -= OnRightDisconnectPressed;
-        }
-
-        if (leftControllerDisconnectAction != null && leftControllerDisconnectAction.action != null)
-        {
-            leftControllerDisconnectAction.action.started -= OnLeftDisconnectPressed;
-        }
-    }
-
-    private void OnRightDisconnectPressed(InputAction.CallbackContext context)
-    {
-        if (ShouldReleaseForHand(true))
-        {
-            Debug.Log("Right trigger: toggle lock/unlock");
-            ToggleFryLock();
-        }
-    }
-
-    private void OnLeftDisconnectPressed(InputAction.CallbackContext context)
-    {
-        if (ShouldReleaseForHand(false))
-        {
-            Debug.Log("Left trigger: toggle lock/unlock");
-            ToggleFryLock();
-        }
-    }
-
-    private void ToggleFryLock()
-    {
-        // Check cooldown
-        if (Time.time - lastToggleTime < toggleCooldownDuration)
-        {
-            return;
-        }
-
-        lastToggleTime = Time.time;
-
-        if (lockedFry != null)
-        {
-            // Already locked, so unlock
-            ReleaseLockedFry();
-            Debug.Log("Fry unlocked");
-        }
-        else if (fryInTrigger != null)
-        {
-            // Not locked but fry is in trigger, so lock it
-            LockFryInBasket(fryInTrigger);
-            Debug.Log("Fry locked");
-        }
-        else
-        {
-            Debug.Log("No fry to lock or unlock.");
-        }
-    }
-
-    private bool ShouldReleaseForHand(bool isRightHand)
-    {
-        // Allow toggle if hand is holding basket or if there's a fry available
-        if (isRightHand && rightControllerHoldingBasket)
-        {
-            return true;
-        }
-
-        if (!isRightHand && leftControllerHoldingBasket)
-        {
-            return true;
-        }
-
-        // Also allow if basket is selected by the hand, even if we're not sure which hand
-        if (basketGrabInteractable != null && basketGrabInteractable.isSelected)
-        {
-            return true;
-        }
-
-        return false;
     }
 }
