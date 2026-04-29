ea0936e279fe446e63ed95dc232341c0e170092d
luisangel <luisangel.esparza2022@gmail.com>
2026-04-28 22:12:55 -0500

    feat: add audio manager and patty sizzle sound on grill

--- VR_Project/Assets/Grill/Grill-Shared/Grill.cs
+++ VR_Project/Assets/Grill/Grill-Shared/Grill.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class Grill : MonoBehaviour
 {
+    [SerializeField] private AudioManager audioManager;
+    private int pattyCount = 0;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio")?.GetComponent<AudioManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Patty patty = other.GetComponent<Patty>();
@@ -9,6 +17,11 @@ public class Grill : MonoBehaviour
         if (patty != null)
         {
             patty.IsOnGrill = true;
+            pattyCount++;
+            if (pattyCount == 1)
+            {
+                audioManager?.PlayGrillLoop(audioManager.pattySizzle);
+            }
             Debug.Log("Patty placed on grill");
         }
     }
@@ -20,6 +33,11 @@ public class Grill : MonoBehaviour
         if (patty != null)
         {
             patty.IsOnGrill = false;
+            pattyCount--;
+            if (pattyCount == 0)
+            {
+                audioManager?.StopGrillLoop();
+            }
             Debug.Log("Patty removed from grill");
         }
     }
